e0585c5ab271804869f9ed2010db455fc25a9218
Mikhail Shcheglakov <m.shcheglakov@gmail.com>
2019-09-22 17:42:46 +0300

    add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  const product = Array(first.length + second.length).fill(0);
+  for (let i = first.length; i--; ) {
+    let carry = 0;
+    for (let j = second.length; j--; ) {
+      product[1 + i + j] += carry + first[i] * second[j];
+      carry = Math.floor(product[1 + i + j] / 10);
+      product[1 + i + j] = product[1 + i + j] % 10;
+    }
+    product[i] += carry;
+  }
+  return product.join('').replace(/^0/, '');
+};
